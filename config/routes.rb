00cfc8e50e149623f34304485c5565927f675a08
Deijah Wilson <deijahwilson1@gmail.com>
2020-04-08 22:03:30 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'authors/index'
+
+  get 'authors/show'
+
+  get 'post/index'
+
+  get 'post/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
